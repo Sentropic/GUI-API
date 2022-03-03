9c012ccddf3d8badaffdbf29e5481f21369e3190
Sentropic <sentropic.sv@gmail.com>
2022-03-03 15:02:09 -0600

    Fix command permissions

--- src/main/java/com/sentropic/guiapi/command/ReloadCommand.java
+++ src/main/java/com/sentropic/guiapi/command/ReloadCommand.java
@@ -13,12 +13,10 @@ public class ReloadCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (args.length == 1 && args[0].equals("reload")) {
-            if (!sender.hasPermission("misc.command.reload")) { return false; }
             GUIAPI.getGUIConfig().reload();
             sender.sendMessage(ChatColor.GREEN+"[GUI API] Reloaded config.yml");
             return true;
         } else if (args.length == 1 && args[0].equals("debug")) {
-            if (!sender.hasPermission("misc.command.debug")) { return false; }
             if (sender instanceof Player) {
                 GUI gui = GUIAPI.getGUIManager().getGUI((Player) sender);
                 gui.setDebug(!gui.isDebugging());
